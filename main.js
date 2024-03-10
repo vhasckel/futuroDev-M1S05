ebd4da3d838bb951d302a68997e594afa08f7d8e
vanessa hasckel <vanessahasckel00@hotmail.com>
2024-03-09 23:31:55 -0300

    evento ao botão de comprar o produto

--- main.js
+++ main.js
@@ -1,35 +1,53 @@
-const text = document.querySelector("p");
+const text = document.querySelector(".display");
 console.log(text);
 
 const search = () => {
   const input = document.querySelector("input").value;
-  const lowerCase = input
+  const inputLowerCase = input
     .toLowerCase()
     .normalize("NFD")
     .replace(/[\u0300-\u036f]/g, "");
 
-  switch (lowerCase) {
+  switch (inputLowerCase) {
     case "1":
     case "morango":
-      text.innerText = `${lowerCase} R$7,99`;
+      text.innerHTML = `<p>${inputLowerCase} R$7,99</p> <button onclick="buyProduct('${inputLowerCase}')">Comprar Produto</button>`;
       break;
     case "2":
     case "banana":
-      text.innerText = `${lowerCase} R$2,97`;
+      text.innerHTML = `<p>${inputLowerCase} R$2,97</p> <button onclick="buyProduct('${inputLowerCase}')">Comprar Produto</button>`;
       break;
     case "3":
     case "manga":
-      text.innerText = `${lowerCase} R$8,00`;
+      text.innerHTML = `<p>${inputLowerCase} R$8,00</p> <button onclick="buyProduct('${inputLowerCase}')">Comprar Produto</button>`;
       break;
     case "4":
     case "maracuja":
-      text.innerText = `${lowerCase} R$3,45`;
+      text.innerHTML = `<p>${inputLowerCase} R$3,45</p> <button onclick="buyProduct('${inputLowerCase}')">Comprar Produto</button>`;
       break;
     case "5":
     case "laranja":
-      text.innerText = `${lowerCase} R$2,25`;
+      text.innerHTML = `<p>${inputLowerCase} R$2,25</p> <button onclick="buyProduct('${inputLowerCase}')">Comprar Produto</button>`;
       break;
     default:
       console.log("o produto nao existe");
   }
+};
+
+let products = [];
+const buyProduct = (product) => {
+  products.push(product);
+  console.log(products);
+  displayProducts();
+};
+
+const displayProducts = () => {
+  const productList = document.getElementById("productList");
+  productList.innerHTML = ""; // Limpar a lista antes de atualizar
+
+  products.forEach((product) => {
+    const listItem = document.createElement("li");
+    listItem.textContent = product;
+    productList.appendChild(listItem);
+  });
 };
